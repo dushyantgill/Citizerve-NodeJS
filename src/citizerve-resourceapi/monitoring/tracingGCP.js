05dcf257e73bc63a51b99a6b2237e3f0f2fb7059
Michael C. Papasevastos <2685124+mpapas@users.noreply.github.com>
2023-01-25 16:16:30 -0600

    Changes to export to Cloud Trace.

---
+++ src/citizerve-resourceapi/monitoring/tracingGCP.js
@@ -0,0 +1,29 @@
+/* tracing.js */
+// Require dependencies
+const opentelemetry = require('@opentelemetry/sdk-node');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const { TraceExporter } = require('@google-cloud/opentelemetry-cloud-trace-exporter');
+const { Resource } = require('@opentelemetry/resources');
+const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
+
+const config = require('../config.json')[process.env.NODE_ENV || 'development'];
+
+const exporter = new TraceExporter({
+  // If you are not in a GCP environment, you will need to provide your
+  // service account key here. See the Authentication section below.
+  // will export all resource attributes that start with "service."
+  resourceFilter: /^service\./  
+});
+
+const sdk = new opentelemetry.NodeSDK({
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'citizerve-resourceapi',
+    [SemanticResourceAttributes.DEPLOYMENT_ENVIRONMENT]: process.env.NODE_ENV,
+  }),
+  traceExporter: exporter,
+  instrumentations: [getNodeAutoInstrumentations()],
+});
+
+sdk.start()
+  .then(() => { console.info('Tracing initialized'); })
+  .catch((error) => console.error('Error initializing tracing', error));

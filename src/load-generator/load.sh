296c6f13d31a98a21dedd0bf908538638aff5108
Michael C. Papasevastos <2685124+mpapas@users.noreply.github.com>
2023-10-12 16:41:56 -0500

    updated load script

--- src/load-generator/load.sh
+++ src/load-generator/load.sh
@@ -17,18 +17,35 @@ delete () {
   curl -X DELETE "$1" -s -w "%{http_code}\n"
 }
 
+generate_unique_id() {
+    uuid=$(printf "%04x%04x-%04x-%04x-%04x-%04x%04x%04x\n" \
+    $RANDOM $RANDOM \
+    $RANDOM \
+    $(($RANDOM & 0x0fff | 0x4000)) \
+    $(($RANDOM & 0x3fff | 0x8000)) \
+    $RANDOM $RANDOM $RANDOM)
+    echo "$uuid"
+}
+
 while true
 do
   echo '\n'
   echo $(($(date +%s) - START)) | awk '{print int($1/60)":"int($1%60)}'
   sleep 1
+
+  unique_id=$(generate_unique_id)
+  # echo "The unique ID is: $unique_id"
+
   echo '\nCreating citizen\n------------'
-  post "$2/api/citizens" '{"citizenId":"dd711c64-0c8d-40ed-88d1-51c534e61d1a","streetAddress":"865 Hamill Avenue","city":"Wilburton","country":"US","givenName":"Everette","phoneNumber":"(918) 710 - 4581","postalCode":"74578","state":"OK","surname":"Mullet"}'
+  # echo "{\"citizenId\":\"${unique_id}\",\"streetAddress\":\"865 Hamill Avenue\",\"city\":\"Wilburton\",\"country\":\"US\",\"givenName\":\"Everette\",\"phoneNumber\":\"(918) 710 - 4581\",\"postalCode\":\"74578\",\"state\":\"OK\",\"surname\":\"Mullet\"}"
+  post "$2/api/citizens" "{\"citizenId\":\"${unique_id}\",\"streetAddress\":\"865 Hamill Avenue\",\"city\":\"Wilburton\",\"country\":\"US\",\"givenName\":\"Everette\",\"phoneNumber\":\"(918) 710 - 4581\",\"postalCode\":\"74578\",\"state\":\"OK\",\"surname\":\"Mullet\"}"
   echo '\nReading citizen\n------------'
   for i in `seq 1 $max`
   do
-    get "$2/api/citizens/dd711c64-0c8d-40ed-88d1-51c534e61d1a"
+    # echo "$2/api/citizens/$unique_id"
+    get "$2/api/citizens/$unique_id"
   done
   echo '\nDeleting citizen\n------------'
-  delete "$2/api/citizens/dd711c64-0c8d-40ed-88d1-51c534e61d1a"
+  # echo "$2/api/citizens/$unique_id"
+  delete "$2/api/citizens/$unique_id"
 done
